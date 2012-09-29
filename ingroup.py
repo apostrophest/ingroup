2d89e3df61f281e6c33e740d600a9b62d5316f97
apostrophest <stephen@chomadoma.net>
2012-09-29 15:25:45 -0400

    I hate Flask/Werkzeug and their routing bullshit.

--- ingroup.py
+++ ingroup.py
@@ -7,18 +7,18 @@ app = Flask(__name__)
 app.debug = True
 
 
-@app.route("/forum")
+@app.route("/")
 def forum_list_view():
     forum_list = forums.forum_list()
     return render_template('forum_list.html', forums=forum_list)
 
 
-@app.route("/forum/<int:id>")
+@app.route("/<int:id>")
 def forum_view():
     pass
 
 
-@app.route("/forum/setup.py")
+@app.route("/setup.py")
 def remote_setup_access():
     # Refuse to serve setup script remotely
     return render_template('error.html', type=404, message='')
